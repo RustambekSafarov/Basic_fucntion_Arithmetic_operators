fccdcafcd3c5ceb96f6c4864517a54730a509d34
Rustambek <rustambeksafarovv@gmail.com>
2022-06-20 16:36:34 +0500

    Redone

--- bin/arithmetic03.dart
+++ bin/arithmetic03.dart
@@ -1,5 +1,11 @@
+int func(a,b){
+    int x = a%b;
+    return x;
+}
 // Create a function called func.
 // CCreate function arguments a and b of type int.
 // Returns the remainder of a and b.
 
-void main() {}
+void main() {
+    print(func(8,5));
+}
