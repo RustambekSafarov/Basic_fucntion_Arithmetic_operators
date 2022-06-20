f02e6d3028c5d20baaccbb0d6eeb08ef653d5851
Rustambek <rustambeksafarovv@gmail.com>
2022-06-20 16:28:56 +0500

    Redone

--- bin/arithmetic02.dart
+++ bin/arithmetic02.dart
@@ -1,7 +1,11 @@
+int divide(a,b){
+    int x = a~/b;
+    return x;
+}
 // Create a function called divide.
 // Create function arguments a and b of type int.
 // Returns the floor division of a and b.
 
 void main() {
-  
+    print(divide(9,2))
 }
