87000ed76dc0ee6b7170f03eecfdb1f5c24cad3f
Rustambek <rustambeksafarovv@gmail.com>
2022-06-20 17:00:49 +0500

    Redone

--- bin/arithmetic06.dart
+++ bin/arithmetic06.dart
@@ -1,7 +1,15 @@
+int func(number){
+    int x = number%10;
+    int y = number~/10;
+    int answer = x*10+y;
+    return answer;
+}
 // Create a function called func.
 // Create a function argument called ‘number’ of type int 
 // the two-digit numbers will be given.
 // Find the reverse of the number and return to a variable called ‘answer’
 // return answer
 
-void main() {}
+void main() {
+    print(func(34));
+}
