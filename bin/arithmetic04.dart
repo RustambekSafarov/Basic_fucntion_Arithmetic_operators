49def8a9c3d933f022eadbd3e59bd77a8e9438c1
Rustambek <rustambeksafarovv@gmail.com>
2022-06-20 16:42:18 +0500

    Redone

--- bin/arithmetic04.dart
+++ bin/arithmetic04.dart
@@ -1,5 +1,11 @@
+int multiple(a,b){
+    int x = a*b;
+    return x;
+}
 // Create a function called multiple.
 // Create function arguments a and b of type int.
 // Return the multiplication of a and b.
 
-void main() {}
+void main() {
+    print(multiple(8,5))
+}
