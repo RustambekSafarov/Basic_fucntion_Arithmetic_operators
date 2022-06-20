ce87658e0ccf7bf16c13df6232a8db375a060990
Rustambek <rustambeksafarovv@gmail.com>
2022-06-20 16:24:53 +0500

    Redone

--- bin/arithmetic01.dart
+++ bin/arithmetic01.dart
@@ -1,8 +1,12 @@
+int add(x,y){
+    int a = x+y;
+    return a;
+}
 // Create a function called add.
 // Create a function argument x of type int.
-// Create a parameter int type y of type int.
-// return sum of two parameters.
+// Create a function argument y of type int.
+// return sum of two functions.
 
 void main() {
-
+    print(add(5,8));
 }
