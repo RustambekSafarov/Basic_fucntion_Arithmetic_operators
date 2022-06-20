27394ca0df04098f68f3d7f39a4d31873453ef88
Rustambek <rustambeksafarovv@gmail.com>
2022-06-20 17:23:32 +0500

    Redone

--- bin/arithmetic09.dart
+++ bin/arithmetic09.dart
@@ -1,7 +1,13 @@
+int func(number){
+    int answer = number % 2;
+    return answer;
+}
 // Create a function called func.
 // Create a function argument called ‘number’ of type int.
 // Find the remainder of the division of number by 2 and assign it to
 // ‘answer’.
 // return the answer.
 
-void main() {}
+void main() {
+    print(func(41));
+}
